e39d370fb461f20d329eb80bfd32a56b4f6c384f
Rahman Adianto <rahman.adianto@gmail.com>
2016-02-28 15:50:24 +0700

    implement matrix multiplication

---
+++ src/multiplication.hpp
@@ -0,0 +1,14 @@
+#ifndef MULTIPLICATION_H
+#define MULTIPLICATION_H
+
+#include <vector>
+
+class multiplication {
+
+public:
+  static std::vector<std::vector<int> > result(const std::vector<std::vector<int> >& A,
+                                 const std::vector<std::vector<int> >& B);
+
+};
+
+#endif
